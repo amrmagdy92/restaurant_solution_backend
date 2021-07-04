7632ffab4243575d9b23b477ad0fc70621538f61
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-07-04 18:22:58 +0200

    Added login and logout methods to the auth_route file

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -4,5 +4,7 @@ const router  = express.Router();
 const authentication_controller = require('../controllers/AUTHENTICATION_CONTROLLER');
 
 router.post('/token', authentication_controller.token);
+router.post('login', authentication_controller.login);
+router.post('/logout', authentication_controller.logout);
 
 module.exports = router;
