44595804a908a9ff0fd1098b3e627df87c142e03
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-07-04 18:21:42 +0200

    Refactored AUTHENTICATION_CONTROLLER to separate token generation from the user authentication

--- controllers/AUTHENTICATION_CONTROLLER.js
+++ controllers/AUTHENTICATION_CONTROLLER.js
@@ -2,36 +2,42 @@ const User = require('../models/USERS')
 const jwt  = require('jsonwebtoken')
 const TOKEN_MODEL = require('../models/REFRESH_TOKEN');
 
+function generateAccessToken(user) {
+    jwt.sign(user,
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.TOKEN_DURATION});
+}
+
 module.exports = {
-    token: async function(req, res) {  
+    token: async function(req, res) {
+        var refresh_token = req.body.token_id;
+        var valid_token = await TOKEN_MODEL.findOne({_id: refresh_token});
+        if (refresh_token == null) return res.sendStatus(401);
+        if(!valid_token) return res.sendStatus(401);
+        jwt.verify(valid_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if(err) return res.sendStatus(403);
+            const access_token = generateAccessToken(user);
+            res.json({ accessToken: access_token});
+        });
+    },
+    logout: async function(req, res) {
+        await TOKEN_MODEL.findOneAndDelete({_id: req.body.token_id});
+        res.sendStatus(201);
+    },
+    login: async function(req, res){
         if (req.body.email && req.body.password) {
-            var email = req.body.email;
-            var password = req.body.password;
-            var user = await User.findOne({ EMAIL: email });
-            var isMatch = await user.comparePassword(password);
-            var token_id;
-            var token;
-            var accessToken;
-            var refreshToken;
+            var user = await User.findOne({EMAIL: req.body.email});
+            var isMatch = await user.comparePassword(req.body.password);
             if (user && isMatch) {
-                var payload = {
-                    id: user.id
-                };
-                token_id = req.body.token_id;
-                if (token_id == null) return res.sendStatus(401);
-                token = await TOKEN_MODEL.findOne({_id : token_id})
-                if(!token) return res.sendStatus(403);
-                jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                    if (err) return res.sendStatus(403);
-                    accessToken = jwt.sign(payload,
-                        process.env.ACCESS_TOKEN_SECRET,
-                        {expiresIn: process.env.TOKEN_DURATION});
-                })
-            } else {
-                res.sendStatus(401);
+                var access_token = generateAccessToken(user);
+                const refresh_token = jwt.sign(user,
+                    process.env.REFRESH_TOKEN_SECRET);
+                await TOKEN_MODEL.create({ token: refresh_token });
+                res.json({
+                    accessToken: access_token,
+                    refreshToken: refresh_token
+                });
             }
-        } else {
-            res.sendStatus(401);
         }
     }
 }
