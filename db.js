09dc854eaa201997314c49f2c3402241d8344d9b
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-07-06 18:43:54 +0200

    changed the database name for production

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = process.env.NODE_ENV === 'development' ? 
                 'mongodb://localhost:27017/restaurant-database-dev' : 
-                'mongodb://localhost:27017/restaurant-database-dev';
+                'mongodb://localhost:27017/restaurant-database-prod';
